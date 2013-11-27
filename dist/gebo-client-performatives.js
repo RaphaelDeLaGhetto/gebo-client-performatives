218f1912cbbcc59d775019ca2b98c85aa89dee1c
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-27 13:42:38 -0700

    Get build 0.0.2

--- dist/gebo-client-performatives.js
+++ dist/gebo-client-performatives.js
@@ -110,7 +110,7 @@
 ;
 (function () {
   'use strict';
-  angular.module('gebo-client-performatives', []).directive('clientReplyRequest', function () {
+  angular.module('gebo-client-performatives').directive('clientReplyRequest', function () {
     return {
       restrict: 'A',
       template: ''
@@ -120,7 +120,7 @@
 ;
 (function () {
   'use strict';
-  angular.module('gebo-client-performatives', []).directive('serverReplyRequest', function () {
+  angular.module('gebo-client-performatives').directive('serverReplyRequest', function () {
     return {
       restrict: 'A',
       template: '<button class="btn btn-small" ng-click="agree(sc._id, $event)">' + '    <span class="glyphicon glyphicon-question-sign"></span></button>' + '<button class="btn btn-small" ng-click="refuse(sc._id, $event)">' + '    <span class="glyphicon glyphicon-thumbs-down"></span></button>' + '<button class="btn btn-small" ng-click="agree(sc._id, $event)">' + '    <span class="glyphicon glyphicon-thumbs-up"></span></button>'

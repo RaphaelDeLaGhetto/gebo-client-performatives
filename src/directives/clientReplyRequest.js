79f31bcc8b197b4a34bdfac8ad32d34a7ac668ab
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-27 13:23:14 -0700

    Get reply-request directives

---
+++ src/directives/clientReplyRequest.js
@@ -0,0 +1,12 @@
+;(function() {
+'use strict';                  
+
+angular.module('gebo-client-performatives', []).
+    directive('clientReplyRequest', function () {
+        return {
+            restrict: 'A',
+            template: '',
+        };
+      });
+  }());
+

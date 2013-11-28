6b7084e5a4cb18f975a408608f6a101c3b4ac3d4
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-28 12:55:12 -0700

    Get build 0.0.2

---
+++ dist/gebo-client-performatives-tpls-0.0.2.js
@@ -0,0 +1,223 @@
+angular.module("gebo-client-performatives", ["gebo-client-performatives.tpls", "gebo-client-performatives.directives","gebo-client-performatives.services"]);
+angular.module("gebo-client-performatives.tpls", []);
+;(function() {
+'use strict';                  
+
+angular.module('gebo-client-performatives').
+    directive('conversationControl', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                type: '=',
+            },
+            template: '<ng-include src="\'templates/\' + type + \'.html\'"></ng-include>',
+         };
+      });
+  }());
+
+
+;(function() {
+'use strict';                  
+
+angular.module('gebo-client-performatives').
+    directive('serverReplyRequest', function () {
+        return {
+            template: '<button class="btn btn-small" ng-click="agree(sc._id, $event)">' +
+                      '    <span class="glyphicon glyphicon-question-sign"></span></button>' +
+                      '<button class="btn btn-small" ng-click="refuse(sc._id, $event)">' +
+                      '    <span class="glyphicon glyphicon-thumbs-down"></span></button>' +
+                      '<button class="btn btn-small" ng-click="agree(sc._id, $event)">' +
+                      '    <span class="glyphicon glyphicon-thumbs-up"></span></button>',
+         };
+      });
+  }());
+
+
+;(function() {
+'use strict';                  
+
+angular.module('gebo-client-performatives', ['ngRoute', 'ngResource']).
+    factory('Request', function () {
+
+        /**
+         * Initiate a request
+         *
+         * @param string
+         * @param string
+         * @param string
+         * @param string
+         *
+         * @return Object
+         */
+        function _request(sender, receiver, action, gebo) {
+            return {
+                    sender: sender,
+                    receiver: receiver,
+                    performative: 'request',
+                    action: action,
+                    gebo: gebo,
+                };
+          };
+
+        /**
+         * Cancel a request
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _cancel(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'cancel request',
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Communicate misunderstanding
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _notUnderstood(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'not-understood ' + sc.performative.split(' ').pop(),
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Refuse an agent's assertion
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _refuse(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'refuse ' + sc.performative.split(' ').pop(),
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Refuse an agent's assertion
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _timeout(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'timeout ' + sc.performative.split(' ').pop(),
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Agree to an agent's assertion
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _agree(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'agree ' + sc.performative.split(' ').pop(),
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Signal failure to perform action
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _failure(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'failure ' + sc.performative.split(' ').pop(),
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Signal that the request action has been performed 
+         *
+         * @param Object - social commitment
+         * @param string - email of sender
+         *
+         * @return Object
+         */
+        function _proposeDischarge(sc, email) {
+            return {
+                    sender: email,
+                    receiver: sc.debtor === email? sc.creditor: sc.debtor,
+                    performative: 'propose ' + sc.performative.split(' ').pop(),
+                    action: sc.action,
+                };
+          };
+
+        /**
+         * Get the name of the appropriate directive
+         *
+         * (This will likely be moved in reorganization)
+         *
+         * @param Object - social commitment
+         * @param string - email of would-be sender
+         *
+         * @return string
+         */
+        function _getDirectiveName(sc, email) {
+
+            // Determine the agent's role
+            var role = 'server';
+            switch(sc.performative) {
+                case 'reply request':
+                    role = sc.debtor === email? 'server': 'client';
+                    break;
+                case 'propose discharge|perform':
+                    role = sc.debtor === email? 'server': 'client';
+                    break;
+                case 'reply propose|discharge|perform':
+                    role = sc.debtor === email? 'client': 'server';
+                    break;
+             }
+
+            // Remove spaces and pipes
+            return role + '-' + sc.performative.replace(' ', '-').replace(/\|/g, '-');
+          };
+
+        return {
+            agree: _agree,
+            cancel: _cancel,
+            failure: _failure,
+            getDirectiveName: _getDirectiveName,
+            notUnderstood: _notUnderstood,
+            proposeDischarge: _proposeDischarge,
+            refuse: _refuse,
+            request: _request,
+            timeout: _timeout,
+        };
+      });
+  }());

30882cdefb248070ab99ba92b0f34eddb6b8add9
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-29 12:27:36 -0700

    Load *.html.js templates

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
           'bower_components/angular-route/angular-route.js',
           'test/**/*.js',
           'src/services/request.js',
-          'src/directives/*.js'
+          'src/directives/**/*.js'
         ],
         
         

017fb1c9df4a5ad1f705e980a634341ae2fd87d4
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-11-29 12:30:56 -0700

    Get directive that loads cached templates

--- src/directives/conversationControl.js
+++ src/directives/conversationControl.js
@@ -1,14 +1,26 @@
 ;(function() {
 'use strict';                  
 
-angular.module('gebo-client-performatives').
-    directive('conversationControl', function () {
-        return {
+angular.module('gebo-client-performatives.conversationControl',
+        ['templates/server-reply-request.html', 'templates/client-reply-request.html']).
+    directive('conversationControl', function ($templateCache) {
+
+    function _link(scope, element, attributes) {
+        console.log('_link');        
+        console.log(attributes);        
+        console.log(element);
+
+        attributes.$observe('type', function(newValue) {
+            console.log('newValue');
+            console.log(newValue);
+//            scope.type = newValue;
+            element.html($templateCache.get('templates/' + attributes.type + '.html'));
+        });
+      };
+
+    return {
             restrict: 'E',
-            scope: {
-                type: '=',
-            },
-            template: '<ng-include src="\'templates/\' + type + \'.html\'"></ng-include>',
+            link: _link,
          };
       });
   }());

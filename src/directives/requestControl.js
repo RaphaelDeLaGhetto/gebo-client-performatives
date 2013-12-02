4bd9a4144cf61e4aa6fd5678f0f826b4e6935d79
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-12-02 13:57:42 -0700

    Get requestControl directive with tests

---
+++ src/directives/requestControl.js
@@ -0,0 +1,54 @@
+;(function() {
+'use strict';                  
+
+angular.module('gebo-client-performatives.request-control',
+        ['gebo-client-performatives.request',
+         'templates/request-control.html']).
+    directive('requestControl', function ($templateCache, Request, $compile) {
+
+    var _link = function(scope, element, attributes) {
+        if (scope.sender && scope.receiver && scope.action && scope.gebo) {
+            element.html($templateCache.get('templates/request-control.html'));
+            $compile(element.contents())(scope);
+        }
+      };
+
+    /**
+     * Controller
+     */
+    var _controller = function($scope, $element, $attrs, $transclude) {
+
+            $attrs.$observe('sender', function(newValue) {
+                $scope.sender = newValue;
+              });
+    
+            $attrs.$observe('receiver', function(newValue) {
+                $scope.receiver = newValue;
+              });
+    
+            $attrs.$observe('action', function(newValue) {
+                $scope.action = newValue;
+              });
+
+            $attrs.$observe('gebo', function(newValue) {
+                $scope.gebo = newValue;
+              });
+
+
+            /**
+             * request
+             */
+            $scope.request = function() {
+                    Request.request($scope.sender, $scope.receiver, $scope.action, $scope.gebo);
+                };
+      };
+
+    return {
+            restrict: 'E',
+            scope: true,
+            link: _link,
+            controller: _controller,
+         };
+      });
+  }());
+
